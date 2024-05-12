74c4966dd14d16da302dc4eac96f3aa21e485057
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-05-12 04:37:17 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,14 +1,18 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { restoreUser } = require("../../utils/auth.js");
+
+
 // const sessionRouter = require('./session.js');
 // const usersRouter = require('./user.js');
-// const { restoreUser } = require("../../utils/auth.js");
+
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null 
   //my note: phase3
-// router.use(restoreUser);//check if the user is authorized user!
+router.use(restoreUser);//check if the user is authorized user!
+
 // router.use('/session', sessionRouter);
   
 // router.use('/users', usersRouter);
