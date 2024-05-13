264fd64f0db551eca4a591c41801d043459e42eb
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-05-13 16:32:41 -0700

    complet Get all Spots owned by the Current User; it works

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { Spot, SpotImage, Review } = require('../../db/models')
+const { requireAuth } = require ('../../utils/auth');
 
-//get all spots
+//Get all Spots
 router.get('/', async(req,res)=> {
 
     const allSpots = await Spot.findAll({
@@ -47,10 +48,65 @@ router.get('/', async(req,res)=> {
         delete spot.dataValues.SpotImages; 
         
     }
-    
+
     return res.status(200).json({Spots: allSpots});
-}
-)
+});
+
+
+//Get all Spots owned by the Current User
+router.get('/current', requireAuth, async(req,res)=> {
+    const currentUserOwnerId = req.user.id;
+    const currentUserSpots = await Spot.findAll({
+        where: { ownerId: currentUserOwnerId},
+        include: 
+        [
+            {model: Review, attributes: ['stars']},
+            {model: SpotImage, attributes: ['url','preview']},
+        ]
+
+    })
+
+    //add avgRating and previewImage to allSpots;delete two other stuff.
+    for (let spot of currentUserSpots) {
+
+        //find AvgRating for each spot, add it to each spot
+        const ratingArr = [];
+        for (let review of spot.Reviews) {
+            ratingArr.push(review.stars);
+        }
+
+        const totalRating = ratingArr.reduce((acc,curr)=>acc+curr);
+        const reviewNum = spot.Reviews.length;
+        result = totalRating/reviewNum;
+
+         //find previewImage for each spot and add it to each spot
+        const image = await SpotImage.findOne({
+            where: {
+                spotId: spot.id,
+                preview: true
+            }
+        });
+
+        if (image) {
+            previewUrl = image.url
+        } else {
+            previewUrl = null;
+        }
+        //Modification in place: Modified spot.dataValues directly to avoid modifying the original Sequelize model instance.
+        spot.dataValues.avgRating = result;
+        spot.dataValues.previewImage = previewUrl;
+
+        delete spot.dataValues.Reviews;
+        delete spot.dataValues.SpotImages; 
+        
+    }
+
+    if (currentUserSpots.length >=1) res.status(200).json({spots: currentUserSpots});
+    else res.json({ message: 'You do not have any spots posted yet!'})
+
+
+})
+
 
 
 module.exports = router;
