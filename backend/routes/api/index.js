2e3403a9017c9ab9b2e826d67b3eec6bf2ef99e2
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-05-16 16:13:37 -0700

    complete Delete a Review Image route; it works

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,7 +5,8 @@ const usersRouter = require('./user.js');
 const spotsRouter = require('./spot.js');
 const reviewsRouter = require("./review.js");
 const bookingsRouter = require('./booking.js');
-const SpotImagesRouter = require("./spot-images.js")
+const spotImagesRouter = require("./spot-images.js");
+const reviewImagesRouter = require('./review-images.js')
 const { restoreUser } = require("../../utils/auth.js");
 
 
@@ -25,7 +26,10 @@ router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
 
-router.use('/spot-images', SpotImagesRouter);
+router.use('/spot-images', spotImagesRouter);
+
+router.use('/review-images', reviewImagesRouter);
+
 
 //Do not remove it yet. You will be using it much later when setting up your frontend.
 router.post('/test', (req, res) => {
--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { Spot, SpotImage} = require('../../db/models')
+const { Review, ReviewImage} = require('../../db/models')
 const { requireAuth } = require ('../../utils/auth');
 
 router.delete('/:imageId', requireAuth, async(req, res, next)=> {
-    const spotImage = await SpotImage.findByPk(req.params.imageId, {
-        include: {model: Spot}
+    const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
+        include: {model: Review}
     });
-    if (!spotImage) {
-        const err = new Error("Spot Image couldn't be found");
-        err.title = "spotImage Not Found";
+    if (!reviewImage) {
+        const err = new Error("Review Image couldn't be found");
+        err.title = "reviewImage Not Found";
         err.status = 404;
         return next(err);
     }
 
-    if (req.user.id !== spotImage.Spot.ownerId) {
+    if (req.user.id !== reviewImage.Review.userId) {
         const err = new Error('Forbidden');
         err.status = 403;
-        err.title = "not allow to delete other person's property"
+        err.title = "not allow to delete other person's reviewImage"
         return next(err);
     }
     
-    await spotImage.destroy();
+    await reviewImage.destroy();
     res.status(200).json({
         message: "Successfully deleted"
     })
-
 });
 
 module.exports = router;
